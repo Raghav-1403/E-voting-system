8529eea3fa364898d2eedc7a065c2a3a9558e95d
Sriraghav P <116968337+Raghav-1403@users.noreply.github.com>
2023-12-16 00:16:04 +0530

    Delete src/mine directory

--- src/mine/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.conf import settings
-
-# Create your models here.
-class count(models.Model):
-    cou=models.CharField(max_length=30)
-class track(models.Model):
-    trace=models.IntegerField()
